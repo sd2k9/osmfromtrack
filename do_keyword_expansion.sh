7aee6acbc2698d8c6fc697fbe9d3ca92285cb207
sd2k9 <robert.lange@s1999.tu-chemnitz.de>
2012-08-19 00:41:37 +0200

    Setup skeletion with command line parsing
    Also shifting the first stage to only parse a set of coordinates, not a GPX file

---
+++ do_keyword_expansion.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Shell script to do simple SVN-like keyword expansion for
+# release.
+# Make sure you don't commit the files AFTER keyword expansion!
+#
+# Copyright (C) 2010 Robert Lange (robert.lange@s1999.tu-chemnitz.de)
+# Licensed under the GNU General Public License, version 2
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+#
+# Supported Keywords are:
+# Number of commits: $LastChangedRevision$
+
+
+# *** Settings
+# Files to change
+FILES="osmfromtrack"
+
+# Get commit count for the directory
+REVCOUNT=$(git log --pretty=oneline |wc -l)
+
+# *** Do $LastChangedRevision: 000$
+echo "Perform LastChangedRevision expansion with value $REVCOUNT"
+for fil in $FILES; do
+    mv $fil ${fil}.bak
+    sed -e "s/\\\$LastChangedRevision\\\$/\\\$LastChangedRevision: ${REVCOUNT}\\\$/g" ${fil}.bak > $fil
+    echo "   $fil replaced"
+done
+
+
+
+# *** Reminder
+echo
+echo "Keyword expansion performed."
+echo "Make sure you don't commit these files now! They are meant for export only."
+echo
